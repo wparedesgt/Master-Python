cdfd38d99af2e1e756dc587afabc9c411f91cc6e
wparedes <wparedesgt@gmail.com>
2021-03-16 08:37:38 -0600

    Entradas y salidas

---
+++ 04-Entrada-Salida/main.py
@@ -0,0 +1,8 @@
+#Entrada
+nombre = input("¿Cual es tu nombre?: ")
+edad = input("¿Cual es tu edad: ")
+
+#Salida
+print(f"Me alegro de conocerte, bienvenido {nombre} \n veo que tienes {int(edad) -1} años")
+
+

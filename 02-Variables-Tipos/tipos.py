b727d46a6b235bdc6b5e0bee8fe44b1d1d89125b
wparedes <wparedesgt@gmail.com>
2021-03-16 07:35:26 -0600

    tipos de datos

---
+++ 02-Variables-Tipos/tipos.py
@@ -0,0 +1,42 @@
+nada = None
+cadena = "Hola soy William Paredes"
+entero = 99
+flotante = 4.2
+booleano = True
+lista = [10, 20, 30, 100, 200]
+listaString = [44, "treinta", 30, "cuarenta"]
+tuplaNoCambia = ("master", "en", "python")
+diccionario = {
+    "nombre": "William", 
+    "apellido": "Paredes", 
+    "curso": "Master en Python"
+}
+rango = range(9)
+dato_byte = b"Probando"
+
+#imprimir la variable
+
+print(nada)
+print(cadena)
+print(entero)
+print(flotante)
+print(booleano)
+print(lista)
+print(listaString)
+print(tuplaNoCambia)
+print(diccionario)
+print(rango)
+print(dato_byte)
+#imprimir tipo de dato
+
+print(type(nada))
+print(type(cadena))
+print(type(entero))
+print(type(flotante))
+print(type(booleano))
+print(type(lista))
+print(type(listaString))
+print(type(tuplaNoCambia))
+print(type(diccionario))
+print(type(rango))
+print(type(dato_byte))

3f53d13b3b159cf142cdded78c9bdee84d85ec08
wparedes <wparedesgt@gmail.com>
2021-03-16 08:29:27 -0600

    Asignacion y Aritmeticos

---
+++ 03-Operadores/aritmeticos.py
@@ -0,0 +1,16 @@
+#Operadores Aritmeticos
+numero1 = 80
+numero2 = 4 #Operador de asignacion "="
+
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1/numero2
+resto = numero1 % numero2
+
+print("*************CALCULADORA*****************")
+print(f"La suma es: {numero1 + numero2}" )
+print(f"La resta es: {resta}" )
+print(f"La multiplicacion es: {multiplicacion}" )
+print(f"La division es: {division}" )
+print(f"El resto es: {resto}" )
+
---
+++ 03-Operadores/asignacion.py
@@ -0,0 +1,42 @@
+#Operadores de Asignacion
+edad = 55
+print(edad)
+
+edad += 5
+edad = edad + 5
+edad -= 5
+
+print(edad)
+
+#Operadores de incremento y decremento
+
+year = 2021
+
+#Incremento
+
+year = year+1
+year += 1
+print(year)
+
+#Decremento
+
+year = year -1
+year -= 1
+print(year)
+
+
+#Preincremento
+
+year = 1+ year
+
+#Predecremento 
+
+year = 1 - year
+
+
+"""
+year++
+year--
+--year
+++year
+"""

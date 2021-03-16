3fbf45db0006a73f920759999c546d6b87fe31f8
wparedes <wparedesgt@gmail.com>
2021-03-16 11:15:10 -0600

    Comienzo de Ejercicios

--- 06-Bucles/bucle-while.py
+++ 06-Bucles/bucle-while.py
@@ -27,3 +27,23 @@ while contador <= 100:
     muestrame = muestrame + ", " + str(contador)
     contador += 1
 print(muestrame)
+
+
+#Ejemplo
+
+print("####EJEMPLO#####")
+numero_usuario = int(input("De que numero quieres la tabla?: "))
+
+if numero_usuario < 1:
+    numero_usuario = 1
+
+    print(f"###Tabla del {numero_usuario} ####")
+
+contador = 1
+
+while contador <= 10:
+    print(f"{numero_usuario} X {contador} = {numero_usuario* contador} ")
+    contador += 1
+else:
+    print("Tabla terminada.")
+
---
+++ 07-ejercicios/ejercicio1.py
@@ -0,0 +1,14 @@
+"""
+Ejercicio
+    - Crear dos variables "pais" y "continente"
+    - Mostrar su valor por pantalla (imprimir)
+    - Poner un comentario diciendo el tipo de dato
+"""
+
+pais = "Guatemala"
+continente = "America"
+year = 2021
+
+print(f"{pais} - {continente} - {str(year)}")
+print(f"El pais es de tipo {type(pais)} y el continente es de tipo {type(continente)}")
+

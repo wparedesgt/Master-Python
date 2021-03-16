4db4bd6e1573ab180fee5aba22ea58175e7d02a2
wparedes <wparedesgt@gmail.com>
2021-03-16 07:19:10 -0600

    change yesterday

---
+++ 01-Hola-Mundo/comentarios.py
@@ -0,0 +1,16 @@
+#Esto me mostrara el texto Hola por la consola o Pantalla
+print("Hola")
+#Estas dos instrucciones muestran texto por pantalla
+print("Mundo")
+print("!!!!")
+
+"""
+
+Esto no se mostrará
+
+print("Esto no se va a mostrrar")
+print("Estará dentro de un comentario Multilenea")
+
+"""
+
+print("Good Bye")

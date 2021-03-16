5f3725760b7341db7e3352c4cefe0976603a6b64
wparedes <wparedesgt@gmail.com>
2021-03-16 07:46:59 -0600

    Curiosidades

---
+++ 02-Variables-Tipos/curiosidades.py
@@ -0,0 +1,23 @@
+mi_texto = '"Master"'
+mi_text02 = "en \"Python\""
+
+texto_unido = mi_texto + " " + mi_text02
+print(texto_unido)
+
+#Salto de Linea
+
+texto_unido = mi_texto + " \n " + mi_text02
+print(texto_unido)
+
+
+#tabulacion
+
+texto_unido = mi_texto + " \t " + mi_text02
+print(texto_unido)
+
+
+#Borra lo anterior
+
+texto_unido = mi_texto + " \r " + mi_text02
+print(texto_unido)
+

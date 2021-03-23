fda20d2a877c4bc9f11568c654d5c54b1acd985b
wparedes <wparedesgt@gmail.com>
2021-03-23 12:57:16 -0600

    Finalizacion de excepciones

--- 15-manejo_errores/main.py
+++ 15-manejo_errores/main.py
@@ -1,6 +1,8 @@
 #Capturar excepciones y manejar errores en codigo
 #suceptible a fallos/errores
 
+"""
+
 try:
     nombre = input("Cual es tu nombre?: ")
 
@@ -15,10 +17,48 @@ else:
 finally:
     print("Fin de la iteración !!!!")
 
+"""
+
+#Multiples excepciones
+
+"""
+
+try:
+
+    numero = int(input("Numero para elevarlo al cuadrado: "))
+    print("El cuadrado es 5" + str(numero*numero))
+except TypeError:
+    print("Debes convertir tus cadenas a enteros!!!")
+except ValueError:
+    print("Introduce un numero!!!")
+except Exception as e:
+    print(type(e))
+    print("Ha ocurrido un error: ", type(e).__name__)
+
+"""
+
+#Excepciones personalizadas o lanzar excepcion
+
+try:
+
+
+    nombre = input("Introduce el nombre: ")
+    edad = int(input("Introduce la edad: "))
+
+    if edad < 5 or edad > 110:
+        raise ValueError("La edad instroduccida no es real") 
+    elif len(nombre) <= 1:
+        raise ValueError("El nombre no esta completo")
+    else:
+        print(f"Bienvenido al Master en Python {nombre} !!!")
+
+except ValueError:
+    print("Introduce los datos correctamente")
+except Exception as e:
+    print("Existe un error")
 
 
 
-    
 
 
 

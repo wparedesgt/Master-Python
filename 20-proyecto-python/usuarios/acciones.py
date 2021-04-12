5736b0926f71887df94199a5efd480cbd59d5306
wparedes <wparedesgt@gmail.com>
2021-04-12 08:50:25 -0600

    Cambios y actualizaciones

--- 20-proyecto-python/usuarios/acciones.py
+++ 20-proyecto-python/usuarios/acciones.py
@@ -15,11 +15,26 @@ class Acciones:
         if registro[0] >= 1:
             print(f"Perfecto {registro[1].nombre} te has registrado con el email {registro[1].email}")
         else:
-            print("No te ha registrado correctamente!!!")
+            print("n\ No te ha registrado correctamente!!!")
 
     def login(self):
         print("\n Identificate en el sistema...")    
-        email = input("Introduce tu emaiil: ")
-        password = input("Introduce tu contraseña: ")
-    
+
+        try:
+
+            email = input("Introduce tu emaiil: ")
+            password = input("Introduce tu contraseña: ")
+            usuario = modelo.Usuario('', '', email, password)
+            login = usuario.identificar()
+
+            if email == login[3]:
+                print(f"n\ Bienvenido {login[1]}, te has registrado en el sistema el: {login[5]} ")
+        except Exception as e:
+            #print(type(e))
+            #print(type(e).__name__)
+            print(f"Login Incorrecto !!!!")
+
+def proximasAcciones(self, usuario):
+    return None
+
 

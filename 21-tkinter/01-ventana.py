301c76123ebbdbc575cc200159de2a38a88b53fe
wparedes <wparedesgt@gmail.com>
2021-04-29 13:21:52 -0600

    Creando classes

--- 21-tkinter/01-ventana.py
+++ 21-tkinter/01-ventana.py
@@ -1,50 +1,81 @@
-#import os
-
-#print(os.getcwd())
-
 #tkinter es un modulo para crear interfaces graficas de usuario
 
 from tkinter import *
 import os.path
 
-# Crear una ventana raiz
+class Programa:
 
-ventana = Tk()
+    def __init__(self):
+        self.titulo = "Master en Python"
+        self.icon = './21-tkinter/imagenes/logotipo.ico'
+        self.icon_alt = '/imagenes/logotipo.ico'
+        self.size = "770x470"
+        self.resizable = False
 
-#Titulo de la ventana
+    def cargar(self):
+        # Crear una ventana raiz
 
-ventana.title("Interfas grafica con Python")
+        ventana = Tk()
+        self.ventana = ventana
 
-#Comprobar si existe un archivo
+        #Titulo de la ventana
 
-ruta_icono = os.path.abspath('/home/wparedes/Documentos/Documentos01/CienciaDatos/Master-Python/Master-Python/21-tkinter/imagenes/logotipo.ico')
+        ventana.title("Prueba")
 
-if not os.path.isfile(ruta_icono):
-    ruta_icono = os.path.abspath('./21-tkinter/imagenes/logotipo.ico')
+        #Comprobar si existe un archivo
 
+        ruta_icono = os.path.abspath(self.icon)
 
-#Icono de la ventana
-
-#ventana.iconbitmap(ruta_icono)
-
-#Mostrar texto en el programa
-
-texto = Label(ventana, text = ruta_icono)
-
-texto.pack()
-
-
-
-# Cambio en el tamaño de la ventna
-
-ventana.geometry("750x450")
-
-#Bloquear el tamaño de la ventana
-
-ventana.resizable(0,0)
+        if not os.path.isfile(ruta_icono):
+            ruta_icono = os.path.abspath(self.icon_alt)
 
 
-# Arrancar y mostrar la ventana hasta que se cierre
+        #Icono de la ventana
 
-ventana.mainloop()
+        #ventana.iconbitmap(ruta_icono)
+
+        #Mostrar texto en el programa
+
+        texto = Label(ventana, text = ruta_icono)
+
+        texto.pack()
+
+
+
+        # Cambio en el tamaño de la ventna
+
+        ventana.geometry(self.size)
+
+        #Bloquear el tamaño de la ventana
+
+        if self.resizable:
+            ventana.resizable(1,1)
+        else:
+            ventana.resizable(0,0)
+
+
+
+        
+
+    def addTexto(self, dato):
+        texto = Label(self.ventana, text = dato)
+        texto.pack()
+
+    def mostrar(self):
+        # Arrancar y mostrar la ventana hasta que se cierre
+
+        self.ventana.mainloop()
+        
+
+#Instanciar mi programa por la clase
+
+programa = Programa()
+programa.cargar()
+programa.addTexto("Prueba de Texto ")
+programa.addTexto("William Paredes")
+programa.mostrar()
+
+
+
+
 

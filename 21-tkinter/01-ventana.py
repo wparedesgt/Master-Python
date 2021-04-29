200f9c31100a302adca441488a60c44f06845d6f
wparedes <wparedesgt@gmail.com>
2021-04-29 11:22:26 -0600

    logotipo

--- 21-tkinter/01-ventana.py
+++ 21-tkinter/01-ventana.py
@@ -1,10 +1,39 @@
+#import os
+
+#print(os.getcwd())
+
 #tkinter es un modulo para crear interfaces graficas de usuario
 
 from tkinter import *
+import os.path
 
 # Crear una ventana raiz
 
 ventana = Tk()
+
+#Titulo de la ventana
+
+ventana.title("Interfas grafica con Python")
+
+#Comprobar si existe un archivo
+
+ruta_icono = os.path.abspath('/home/wparedes/Documentos/Documentos01/CienciaDatos/Master-Python/Master-Python/21-tkinter/imagenes/logotipo.ico')
+
+if not os.path.isfile(ruta_icono):
+    ruta_icono = os.path.abspath('./21-tkinter/imagenes/logotipo.ico')
+
+
+#Icono de la ventana
+
+#ventana.iconbitmap(ruta_icono)
+
+#Mostrar texto en el programa
+
+texto = Label(ventana, text = ruta_icono)
+
+texto.pack()
+
+
 
 # Cambio en el tamaño de la ventna
 

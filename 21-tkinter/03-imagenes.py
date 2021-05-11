d0e547d1ca07524454a4155f4211dff57e3597bc
wparedes <wparedesgt@gmail.com>
2021-05-11 09:09:15 -0600

    cambios en python

---
+++ 21-tkinter/03-imagenes.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import os
+
+
+
+ventana = Tk()
+ventana.geometry("700x500")
+Label(ventana, text="Hola soy William").pack(anchor=W)
+print(os.getcwd())
+imagen = Image.open('21-tkinter/imagenes/lobo-gris-800.jpg')
+render = ImageTK.PhotoImage(imagen)
+Label(ventana, image=render).pack()
+ventana.mainloop()

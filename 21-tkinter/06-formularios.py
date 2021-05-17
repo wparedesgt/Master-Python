b1fa03d2bde83ef43ea8702382d4b4bf8cf6ed7b
wparedes <wparedesgt@gmail.com>
2021-05-17 10:21:53 -0600

    Terminando formularios

--- 21-tkinter/06-formularios.py
+++ 21-tkinter/06-formularios.py
@@ -15,7 +15,7 @@ encabezado.config(
 )
 #encabezado.pack(side=LEFT, anchor=NW, fill=X, expand=YES)
 #encabezado.pack(side=LEFT, anchor=NW)
-encabezado.grid(row=0, column=0, columnspan=3, sticky=W)
+encabezado.grid(row=0, column=0, columnspan=6, sticky=W)
 
 #Label para el campo (Nombre)
 label = Label(ventana, text="Nombre")
@@ -39,7 +39,35 @@ campo_texto = Entry(ventana)
 campo_texto.grid(row=2, column=1, padx=5, pady=5)
 
 
+#Label para el campo (Descripcion)
+label = Label(ventana, text="Descripcion")
+label.grid(row=3, column=0, sticky=NE, padx=5, pady=5)
 
+
+#Campo de texto grande para la descripcion
+
+campo_grande = Text(ventana)
+campo_grande.grid(row=3, column=1)
+campo_grande.config(
+    width=30, 
+    height=5,
+    font=("Arial", 12), 
+    padx=15, 
+    pady=15
+    )
+
+#Boton
+Label(ventana).grid(row=4, column=1)
+
+
+boton = Button(ventana, text="Enviar")
+boton.grid(row=5, column=1, sticky=W)
+boton.config(
+    padx=10, 
+    pady=15,
+    bg="green", 
+    fg="white"
+    )
 
 ventana.mainloop()
 

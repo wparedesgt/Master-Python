b070858d638f4d22a48888ad0ea73be3a3e77ac0
wparedes <wparedesgt@gmail.com>
2021-05-31 11:11:12 -0600

    cambios

--- 25-django/AprendiendoDjango/miapp/views.py
+++ 25-django/AprendiendoDjango/miapp/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+#MVC = Modelo Vista Controlador -> Acciones (metodos)
+#MVT =Modelo Vista Template (En djando se llama template a la vista y el controlador es la vista) -> (metodos)
+
+def hola_mundo(request):
+    return HttpResponse("Hola mundo con Django!!!!")

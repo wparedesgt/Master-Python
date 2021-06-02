7da6a6b03106cbb67d8e83a7c8fbb334a9bcabc0
wparedes <wparedesgt@gmail.com>
2021-06-02 10:12:19 -0600

    logo

--- 25-django/AprendiendoDjango/AprendiendoDjango/urls.py
+++ 25-django/AprendiendoDjango/AprendiendoDjango/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-#from miapp import views 
+#from django.conf import settings
+#from django.conf.urls.static import static
 import miapp.views
 
 

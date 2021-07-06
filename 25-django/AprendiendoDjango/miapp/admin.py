b7a206f6290e1e3910acb514a0c2ee9d2f313c4e
wparedes <wparedesgt@gmail.com>
2021-07-06 15:58:16 -0600

    Cambios

--- 25-django/AprendiendoDjango/miapp/admin.py
+++ 25-django/AprendiendoDjango/miapp/admin.py
@@ -3,5 +3,8 @@ from .models import Article, Category
 
 # Register your models here.
 
+#class ArticleAdmin(admin.ModelAdmin):
+#    readonly_fields = ('created_at', 'updated_at')
+
 admin.site.register(Article)
 admin.site.register(Category)
--- 25-django/AprendiendoDjango/miapp/models.py
+++ 25-django/AprendiendoDjango/miapp/models.py
@@ -3,16 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    title = models.CharField(max_length=150)
-    content = models.TextField()
-    image = models.ImageField(default='null')
-    public = models.BooleanField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=150, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Contenido")
+    image = models.ImageField(default='null',verbose_name="Imagen")
+    public = models.BooleanField(verbose_name="Publicado")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    update_at = models.DateTimeField(auto_now_add=True, verbose_name="Modificado")
 
     class Meta:
         verbose_name = "Articulo"
         verbose_name_plural = "Articulos"
+        ordering = ['-id']
+
+    def __str__(self) -> str:
+        if self.public:
+            publico = 'Publicado'
+        else:
+            publico = 'Privado'
+
+        return f"{self.title}  ({publico})"
 
 
 

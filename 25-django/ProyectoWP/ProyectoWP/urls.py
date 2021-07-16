838423f779aed0953896e6f98a4fab6815d9e79a
wparedes <wparedesgt@gmail.com>
2021-07-16 13:42:06 -0600

    Inicio

--- 25-django/ProyectoWP/ProyectoWP/urls.py
+++ 25-django/ProyectoWP/ProyectoWP/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name = "index"),
+    path('inicio/', views.index, name = "inicio"),
+    path('sobre-mi/', views.about, name= "about"),
+
 ]

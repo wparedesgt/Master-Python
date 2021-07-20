10d42bdc79b7b89c8957fa98c77ef0cf97b43913
wparedes <wparedesgt@gmail.com>
2021-07-20 10:10:47 -0600

    Cambios en Paginas

--- 25-django/ProyectoWP/pages/admin.py
+++ 25-django/ProyectoWP/pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Page
 
 # Register your models here.
+
+admin.site.register(Page)
+
+title = "Proyecto William Paredes"
+subtitle = "Panel de Gestion"
+
+admin.site.site_header = title
+admin.site.site_title = title
+admin.site.index_title = subtitle

c4f2318921b52c5944a33ca5c4ad97d562434b47
wparedes <wparedesgt@gmail.com>
2021-07-20 10:46:16 -0600

    Cambios

--- 25-django/ProyectoWP/ProyectoWP/settings.py
+++ 25-django/ProyectoWP/ProyectoWP/settings.py
@@ -64,6 +64,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+             #   'pages.contex_processors.get_pages',
+
             ],
         },
     },

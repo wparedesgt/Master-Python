11f3e5a67c2250c52758e7fd211d174d68cd920b
wparedes <wparedesgt@gmail.com>
2021-07-29 12:17:18 -0600

    ckeditor

---
+++ 25-django/ProyectoWP/mainapp/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+   path('', views.index, name = "index"),
+    path('inicio/', views.index, name = "inicio"),
+     
+]
---
+++ 25-django/ProyectoWP/pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('pagina/<str:slug>', views.page, name="page")
+     
+]
+

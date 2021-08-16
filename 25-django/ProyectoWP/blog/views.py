681ae75692c392793f6d552577a5df2c728187ad
wparedes <wparedesgt@gmail.com>
2021-08-16 11:54:16 -0600

    Cambios

--- 25-django/ProyectoWP/blog/views.py
+++ 25-django/ProyectoWP/blog/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from blog.models import Category, Article
 
 # Create your views here.
 def list(request):
-
+    #Sacar los articulos
     articles = Article.objects.all()
+    #Paginar Articulos
+    paginator = Paginator(articles, 4)
+    #Recoger No. de Pagina
+    page = request.GET.get('page')
+    page_articles = paginator.get_page(page)
+
 
     return render(request, 'articles/list.html', {
         'title':'Articulos', 
-        'articles': articles
+        'articles': page_articles
     })
 
 def category(request, category_id):

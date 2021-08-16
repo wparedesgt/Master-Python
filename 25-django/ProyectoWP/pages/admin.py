a07c13a273bc7c29a593672724e835cf29b16ada
wparedes <wparedesgt@gmail.com>
2021-08-16 10:29:46 -0600

    paginacion

--- 25-django/ProyectoWP/pages/admin.py
+++ 25-django/ProyectoWP/pages/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from .models import Page
 
+#Configuracion Extra
+
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('title', 'content')
+    list_filter = ('visible',)
+    list_display = ('title', 'visible', 'created_at')
+    ordering = ('-created_at',)
+
+
 # Register your models here.
 
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 title = "Proyecto William Paredes"
 subtitle = "Panel de Gestion"

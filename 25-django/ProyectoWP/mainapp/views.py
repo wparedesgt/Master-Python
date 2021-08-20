d90fbf98ee41e4c488dad2b8c0902cf74169b8dd
wparedes <wparedesgt@gmail.com>
2021-08-20 08:16:33 -0600

    login de usuarios

--- 25-django/ProyectoWP/mainapp/views.py
+++ 25-django/ProyectoWP/mainapp/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from mainapp.forms import Register_Form
 
 # Create your views here.
 
@@ -11,3 +14,26 @@ def about(request):
     return render(request, 'mainapp/about.html',{
         'title': 'Sobre Mi'
     })
+
+def register_page(request):
+
+    register_form = Register_Form()
+
+    if request.method == 'POST':
+        register_form = Register_Form(request.POST)
+
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, 'Te has registrado correctamente')
+
+            return redirect('/inicio')
+
+
+
+
+    return render(request, 'users/register.html',{
+        'title': 'Registro',
+        'register_form': register_form
+    })
+
+
